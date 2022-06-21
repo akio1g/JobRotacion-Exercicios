250601b7845c22b85157a2a1022f9d8234316d44
Gabriel Akio <71736094+akio1g@users.noreply.github.com>
2022-06-21 11:25:28 -0300

    Exercício 3 - Faturamento XML (Entidade)

---
+++ entity/Faturamento.java
@@ -0,0 +1,37 @@
+package entity;
+
+public class Faturamento {
+	private int dia;
+	private double valor;
+	
+	public Faturamento() {}
+
+	public Faturamento(int dia, double valor) {
+		super();
+		this.dia = dia;
+		this.valor = valor;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Faturamento [dia: " + dia + "------ valor= R$" + String.format("%.2f", valor) + "]";
+	}
+	
+	
+}
